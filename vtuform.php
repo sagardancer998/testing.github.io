cb84e5bfd3060a35513e75d23698f88c13773371
sagardancer998 <sagardancer998@gmail.com>
2018-03-28 20:26:19 -0700

    test

---
+++ vtuform.php
@@ -0,0 +1,45 @@
+<html><meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
+<center><head><h2>VTU STUDENTS REQUIRMENTS</h2></head></center>
+<style>
+ body {
+    background-color : #484848;
+    margin: 0;
+    padding: 0;
+}
+h1 {
+    color : #000000;
+    text-align : center;
+    font-family: "SIMPSON";
+}
+form {
+    width: 300px;
+    margin: 0 auto;
+}
+</style><center>
+<iframe data-aa='874657' src='//ad.a-ads.com/874657?size=234x60' scrolling='no' style='width:234px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+</center>
+<form action="vtu1.php" method="POST">
+<h1> 
+<table>
+<tr>
+<td>Enter Name : </td>
+<td><input type="text" name="name" placeholder="Name" required></td></tr>
+<tr>
+<td>Enter USN: </td>
+<td><input type="text" name="usn" placeholder="USN"required></td></tr>
+<tr>
+<td>DOB:</td>
+ <td> <input type="date" name="bday"></td></tr>
+<td>Enter Mail Id : </td>
+<td><input type="email" name="mail" placeholder="Mail Id"required></td></tr>
+<tr>
+<td>Enter What'sapp No(Optional): </td>
+<td><input type="text" name="wno" placeholder="What'sapp No "></td></tr>
+<tr>
+<td>Enter REQUIRMENTS:</td>
+<td><textarea name="comments" id="comments" placeholder="Enter your needs" style="font-family:sans-serif;font-size:1.2em;" required>
+</textarea> </table>
+ </tr></h1>
+<input type="submit" name="submit">
+</form>
+</html>
